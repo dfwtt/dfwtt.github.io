74870ad08808660a4b6b1d6b1588ee29d21b0145
minimalentry <minimalentry@gmail.com>
2017-05-10 20:26:48 -0500

    Menu now collapses at mobile size

--- src/environment/components/Navbar/Navbar.jsx
+++ src/environment/components/Navbar/Navbar.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-import { Container, Segment, Header, Grid, Image, Button, Menu } from 'semantic-ui-react';
+import { Menu } from 'semantic-ui-react';
 import './Navbar.css';
 
 export default class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { isCollapsed: false };
+        this.adjustDisplay = this.adjustDisplay.bind(this);
+        this.setResizeListener();
+    }
+
+    setResizeListener() { window.addEventListener('resize', this.adjustDisplay); }
+
+    adjustDisplay() {
+        const { isCollapsed } = this.state;
+        const mobileSized = this.getBrowserWidth() < 680;
+        if (mobileSized && !isCollapsed) this.toggleCollapsed(true);
+        if (!mobileSized && isCollapsed) this.toggleCollapsed(false);
+    }
+
+    toggleCollapsed(isCollapsed) { this.setState({ isCollapsed }); }
+
+    getBrowserWidth() { return window.innerWidth; }
+
     render () {
+        const { isCollapsed } = this.state;
         const { active } = this.props;
+        const largeMenuStyle = { display: isCollapsed ? 'none' : 'flex' };
         return (
-            <Menu className='Navbar' fixed='top' stackable>
-                <Menu.Item active={active === null} href='#top'>
-                    <img src='/images/dfwtt.gif'/>
-                </Menu.Item>
+            <div>
+                <Menu style={largeMenuStyle} className='Navbar' fixed='top'>
+                    <Menu.Item active={active === null} href='#top'>
+                        <img src='/images/dfwtt.gif'/>
+                    </Menu.Item>
                     <Menu.Item active={active === 'news'} href='#news'>
                         News
                     </Menu.Item>
@@ -22,7 +45,8 @@ export default class Navbar extends Component {
                     <Menu.Item active={active === 'membership'} href='#membership'>
                         Membership
                     </Menu.Item>
-            </Menu>
+                </Menu>
+            </div>
         );
     }
 }
